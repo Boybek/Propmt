e091dcd8a9e3c80f7b2d82d06e99283e4718c516
Bobomurodov Oybek <bobomurodovoybek320@gmail.com>
2023-01-16 15:17:18 +0500

    added index.html and script.js

---
+++ script.js
@@ -0,0 +1,31 @@
+ let name = prompt('What is your name?');
+ let surname = prompt('what is your surname?');
+ let age = prompt('How is your age?');
+
+ let user = {}
+
+ user.name = 'some'
+
+ console.log(resultConfirm)
+
+  let sport = prompt('Do you like football?');
+  let address = prompt('Where do you live?');
+  let how  = prompt('How are you?');
+
+  let user2 = {}
+
+  user2.name = 'somes'
+
+  console.log(resultConfirm)
+
+
+
+ let pet = prompt('Do you have pupies?');
+ let coffee  = prompt('Do you like caffe?');
+ let travelling = prompt('Do you like travelling?');
+
+ let user3 = {}
+
+ user3.name = 'somes'
+
+ console.log(resultConfirm)
